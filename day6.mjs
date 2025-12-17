20132aa483e671283f000f02acf13428c8ee6851
Ryan Hadlow <22102278+rh569@users.noreply.github.com>
2025-12-17 09:48:37 +0000

    day 6 part 2

--- day6.mjs
+++ day6.mjs
@@ -23,7 +23,44 @@ const input = getInput('day6');
 })();
 
 (function part2() {
-    const a = input.trimEnd()
+    const lines = input.split('\n').map(l => l.split(''))
+    lines.pop() // not using trimEnd because the whitespace is significant. This gets rid of the empty element.
 
-    // console.log('Part 2:', a)
+    let sums = []
+    let sum = []
+
+    // step through each character from RTL
+    for (let j = lines[0].length - 1; j >= 0; j--) {
+        let num = ''
+        for (let i = 0; i < lines.length; i++) {
+            const c = lines[i][j];
+
+            if (i < lines.length - 1) {
+                num += c
+                continue
+            }
+
+            // end of number
+            sum.push(num / 1)
+            num = ''
+
+            // end of sum
+            if (c === '+' || c === '*') {
+                sum.push(c)
+                sums.push(sum)
+                sum = []
+                if (j > 1) j--;
+            }
+        }
+    }
+
+    const grandTotal = sums
+        .map((sum) => {
+            const [nums, op] = [sum.slice(0, -1), sum[sum.length - 1]]
+            const [accFn, init] = op === '+' ? [(acc, v) => acc + v, 0] : [(acc, v) => acc * v, 1]
+            return nums.reduce(accFn, init)
+        })
+        .reduce((a, v) => a + v)
+
+    console.log('Part 2:', grandTotal)
 })();
